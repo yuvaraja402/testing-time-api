d96fef36ff3b2508bc73dcbb92f48700c133fa75
Yuvaraja <yuvaraja@Yuvarajas-MacBook-Pro.local>
2024-02-07 19:08:19 +0000

    testing the tests

--- test.py
+++ test.py
@@ -6,9 +6,11 @@ def test_get_time():
         response = client.get('/time')
         assert response.status_code == 200
         assert 'Time' in response.json
+        print(response.json)
 
 def test_time_by_region():
     with app.test_client() as client:
-        response = client.get('/time/America')
+        response = client.get('/time/AmeRICA')
         assert response.status_code == 200
         assert 'America' in response.json
+        print(response.json)
